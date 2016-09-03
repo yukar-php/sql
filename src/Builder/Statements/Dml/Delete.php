8827e6f6c393fd69beaa01c39c4c75cc7af7b792
h-sugawara <nnfxt540@gmail.com>
2016-09-03 23:28:02 +0900

    [Add] implement Delete class
    Add the Delete class file and some files related with it to projects.

---
+++ src/Builder/Statements/Dml/Delete.php
@@ -0,0 +1,74 @@
+<?php
+namespace Yukar\Sql\Builder\Statements\Dml;
+
+use Yukar\Sql\Builder\Statements\Phrases\From;
+use Yukar\Sql\Interfaces\Builder\Objects\ICondition;
+use Yukar\Sql\Interfaces\Builder\Objects\ISqlQuerySource;
+
+/**
+ * テーブルのデータを除去する削除の問い合わせクエリの機能を提供します。
+ *
+ * @author hiroki sugawara
+ */
+class Delete extends BaseConditionalDMLQuery
+{
+    /**
+     * Delete クラスの新しいインスタンスを初期化します。
+     *
+     * @param From $from 削除の問い合わせクエリの対象となる表やサブクエリ
+     */
+    public function __construct(From $from)
+    {
+        $this->setSqlQuerySource($from);
+    }
+
+    /**
+     * SQLのデータ操作言語の問い合わせクエリの書式を取得します。
+     *
+     * @return string SQLのデータ操作言語の問い合わせクエリの書式
+     */
+    public function getQueryFormat(): string
+    {
+        return 'DELETE %s %s';
+    }
+
+    /**
+     * SQLのデータ操作言語の対象となる表やサブクエリを設定します。
+     *
+     * @param ISqlQuerySource $sql_query_source SQLのデータ操作言語の対象となる表やサブクエリ
+     */
+    public function setSqlQuerySource(ISqlQuerySource $sql_query_source)
+    {
+        if ($sql_query_source instanceof From === false) {
+            throw new \InvalidArgumentException();
+        }
+
+        parent::setSqlQuerySource($sql_query_source);
+    }
+
+    /**
+     * 問い合わせクエリの条件式を設定します。
+     *
+     * @param ICondition $condition 問い合わせクエリの条件式
+     *
+     * @throws \InvalidArgumentException 引数 condition の要素が空の場合
+     *
+     * @return Delete 条件式を設定した状態のオブジェクトのインスタンス
+     */
+    public function setWhere(ICondition $condition): Delete
+    {
+        $this->setWhereCondition($condition);
+
+        return $this;
+    }
+
+    /**
+     * SQLのデータ操作言語の問い合わせクエリを文字列として取得します。
+     *
+     * @return string SQLのデータ操作言語の問い合わせクエリ
+     */
+    public function __toString(): string
+    {
+        return rtrim(sprintf($this->getQueryFormat(), $this->getSqlQuerySource(), $this->getWhereString()));
+    }
+}
