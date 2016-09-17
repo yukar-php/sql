3890bb2d87cc14bdd38e0330789149ef7eba6495
h-sugawara <nnfxt540@gmail.com>
2016-09-17 14:07:00 +0900

    [Change] move class const values

--- src/Builder/Operators/AtCondition/BaseComparableOperator.php
+++ src/Builder/Operators/AtCondition/BaseComparableOperator.php
@@ -10,28 +10,6 @@ use Yukar\Sql\Interfaces\Builder\Operators\IComparableOperator;
  */
 abstract class BaseComparableOperator extends BaseConditionContainable implements IComparableOperator
 {
-    /** 式の演算子が「=（等しい）」であることを示す定数 */
-    const SIGN_EQ = 1;
-    /** 式の演算子が「!=（等しくない）」であることを示す定数 */
-    const SIGN_NE = 2;
-    /** 式の演算子が「>（より大きい）」であることを示す定数 */
-    const SIGN_GT = 3;
-    /** 式の演算子が「>=（以上）」であることを示す定数 */
-    const SIGN_AO = 4;
-    /** 式の演算子が「<（より小さい）」であることを示す定数 */
-    const SIGN_LT = 5;
-    /** 式の演算子が「<=（以下）」であることを示す定数 */
-    const SIGN_OU = 6;
-
-    const SIGNS = [
-        self::SIGN_EQ => '=',
-        self::SIGN_NE => '<>',
-        self::SIGN_GT => '>',
-        self::SIGN_AO => '>=',
-        self::SIGN_LT => '<',
-        self::SIGN_OU => '<=',
-    ];
-
     private $sign = self::SIGN_EQ;
 
     /**
